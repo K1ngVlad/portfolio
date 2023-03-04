d6f65e2c3aeec9dc32450afd8ec8ae8295cbce75
K1ngVlad <kingvlad3008@gmail.com>
2023-03-04 15:26:09 +0400

    update api

--- src/api/portfolio.js
+++ src/api/portfolio.js
@@ -1,36 +1,36 @@
 import euclid from '../assets/portfolio/euclid.png';
 import courses from '../assets/portfolio/courses.png';
-import mySign from '../assets/portfolio/my-sign.png';
-import planAndmanage from '../assets/portfolio/plan-and-manage.png';
+import theater from '../assets/portfolio/theater.png';
+import home from '../assets/portfolio/home.png';
 
 const portfolio = [
   {
     id: 0,
+    name: 'Большой театр',
+    img: theater,
+    url: 'https://k1ngvlad.github.io/theater/',
+    gitHub: 'https://github.com/K1ngVlad/theater',
+  },
+  {
+    id: 1,
+    name: 'Дом лучшей еды',
+    img: home,
+    url: 'https://k1ngvlad.github.io/home/',
+    gitHub: 'https://github.com/K1ngVlad/home',
+  },
+  {
+    id: 2,
     name: 'Евклид / Проектные системы',
     img: euclid,
     url: 'https://k1ngvlad.github.io/full_gulp/dist/pages/',
     gitHub: 'https://github.com/K1ngVlad/full_gulp',
   },
   {
-    id: 1,
+    id: 3,
     name: 'Первый курс по компьютерной сборке',
     img: courses,
     url: 'https://k1ngvlad.github.io/layout/',
     gitHub: 'https://github.com/K1ngVlad/layout',
-  },
-  {
-    id: 2,
-    name: 'MySign Studio',
-    img: mySign,
-    url: 'https://k1ngvlad.github.io/public/',
-    gitHub: 'https://github.com/K1ngVlad/public',
-  },
-  {
-    id: 3,
-    name: 'Plan and manage',
-    img: planAndmanage,
-    url: 'https://k1ngvlad.github.io/Plan_and_manage/',
-    gitHub: 'https://github.com/K1ngVlad/Plan_and_manage',
   },
 ];
 
